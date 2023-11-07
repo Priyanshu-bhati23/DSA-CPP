d9b3f04d8191cef84b2193059c0cd42b9bd602af
Priyanshu Bhati <138615018+Priyanshu-bhati23@users.noreply.github.com>
2023-11-07 21:38:16 +0530

    Add files via upload

---
+++ basics.cpp
@@ -0,0 +1,25 @@
+#include<iostream>//preprocessor directive
+using namespace std;
+int main(){
+    cout << "namaste duniya"<< endl;
+int n;
+cout<<"enter a number"<<endl;
+cin>>n;
+cout<<n<<endl;
+float num,dub;
+cin>>num;
+cin>>dub;
+cout<<num<<endl;
+cout<<dub<<endl;
+char ch='A';
+cout<<ch<<endl;
+//to find size on bits of  a datatype
+int k=sizeof(num);
+cout<<num<<endl;
+int l=sizeof(n);
+cout<<l<<endl;
+int m=sizeof(ch);
+cout<<m<<endl;
+int o=sizeof(dub);
+cout<<o<<endl;
+}
